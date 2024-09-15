13aa7b415be7ccb1ddcbbb903e1a6de182c4bc0b
hirajun1106 <merumameta1106@gmail.com>
2024-09-15 23:19:09 +0900

    main.go作成

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"gin-tutorial/controllers"
+	"gin-tutorial/models"
+	"gin-tutorial/repositories"
+	"gin-tutorial/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	items := []models.Item{
+		{ID: 1, Name: "商品1", Price: 1000, Description: "商品説明1", SoldOut: false},
+		{ID: 2, Name: "商品2", Price: 2000, Description: "商品説明2", SoldOut: true},
+		{ID: 3, Name: "商品3", Price: 3000, Description: "商品説明3", SoldOut: false},
+	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
+	r := gin.Default()
+	r.GET("/items", itemController.FindAll)
+	r.Run("localhost:8080")
+}
