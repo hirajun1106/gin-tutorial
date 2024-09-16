abb15b8079eb6cc527332a4671c809ce2aedd73f
hirajun1106 <merumameta1106@gmail.com>
2024-09-16 13:38:45 +0900

    Updateを追加

--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -9,6 +9,7 @@ type IItemRepository interface {
 	FindAll() (*[]models.Item, error)
 	FindById(itemId uint) (*models.Item, error)
 	Create(newItem models.Item) (*models.Item, error)
+	Update(updateItem models.Item) (*models.Item, error)
 }
 
 type ItemMemoryRepository struct {
@@ -23,9 +24,9 @@ func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
 	return &r.items, nil
 }
 
-func (r *ItemMemoryRepository) FindById(itemId uint) (*models.Item, error){
-	for _, v:= range r.items {
-		if  v.ID == itemId {
+func (r *ItemMemoryRepository) FindById(itemId uint) (*models.Item, error) {
+	for _, v := range r.items {
+		if v.ID == itemId {
 			return &v, nil
 		}
 	}
@@ -36,4 +37,14 @@ func (r *ItemMemoryRepository) Create(newItem models.Item) (*models.Item, error)
 	newItem.ID = uint(len(r.items) + 1)
 	r.items = append(r.items, newItem)
 	return &newItem, nil
+}
+
+func (r *ItemMemoryRepository) Update(updateItem models.Item) (*models.Item, error) {
+	for i, v := range r.items {
+		if v.ID == updateItem.ID {
+			r.items[i] = updateItem
+			return &r.items[i], nil
+		}
+	}
+	return nil, errors.New("Unexpected error")
 }
