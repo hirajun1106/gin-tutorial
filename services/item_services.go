b6ea882001b381d218ba84727790341465e7c1ba
hirajun1106 <merumameta1106@gmail.com>
2024-09-16 13:38:54 +0900

    Updateを追加

--- services/item_services.go
+++ services/item_services.go
@@ -8,8 +8,9 @@ import (
 
 type IItemService interface {
 	FindAll() (*[]models.Item, error)
-	FindById(itemId uint) (*models.Item, error) 
+	FindById(itemId uint) (*models.Item, error)
 	Create(CreateItemInput dto.CreateItemInput) (*models.Item, error)
+	Update(itemId uint, updateItemInput dto.UpdateItemInput) (*models.Item, error)
 }
 
 type ItemService struct {
@@ -24,16 +25,40 @@ func (s *ItemService) FindAll() (*[]models.Item, error) {
 	return s.repository.FindAll()
 }
 
-func (s *ItemService) FindById(itemId uint) (*models.Item, error){
+func (s *ItemService) FindById(itemId uint) (*models.Item, error) {
 	return s.repository.FindById(itemId)
 }
 
-func (s *ItemService) Create(createItemInput dto.CreateItemInput) (*models.Item, error){
-	newItem := models.Item {
-		Name: createItemInput.Name,
-		Price: createItemInput.Price,
+func (s *ItemService) Create(createItemInput dto.CreateItemInput) (*models.Item, error) {
+	newItem := models.Item{
+		Name:        createItemInput.Name,
+		Price:       createItemInput.Price,
 		Description: createItemInput.Description,
-		SoldOut: false,
+		SoldOut:     false,
 	}
 	return s.repository.Create(newItem)
+}
+
+func (s *ItemService) Update(itemId uint, updateItemInput dto.UpdateItemInput) (*models.Item, error) {
+	targetItem, err := s.FindById(itemId)
+	if err != nil {
+		return nil, err
+	}
+
+	if updateItemInput.Name != nil {
+		targetItem.Name = *updateItemInput.Name
+	}
+
+	if updateItemInput.Price != nil {
+		targetItem.Price = *updateItemInput.Price
+	}
+
+	if updateItemInput.Description != nil {
+		targetItem.Description = *updateItemInput.Description
+	}
+
+	if updateItemInput.SoldOut != nil {
+		targetItem.SoldOut = *updateItemInput.SoldOut
+	}
+	return s.repository.Update(*targetItem)
 }
